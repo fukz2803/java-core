d36d0246c8cd17ff6519e4da7bfc65cb8780c45f
Kiên <fukz2803@gmail.com>
2021-12-20 09:42:45 +0700

    fix-bai-tap1

--- java-core-1/src/App.java
+++ java-core-1/src/App.java
@@ -1,17 +1,37 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        
+        // cong tru nhan chia 2 so
         System.out.println(Calculator.cong(3, 5));
         System.out.println(Calculator.tru(84, 5));
         System.out.println(Calculator.nhan(21, 7));
         System.out.println(Calculator.chia(50, 5));
 
+
+        // BMI tra ve kieu double
         double canNang = 53;
         double chieuCao = 1.69;
         double bmical = canNang/(chieuCao*chieuCao);
         System.out.println("Ket qua BMI cua ban la " +bmical);
 
-        System.out.println(bmi.bmi2(1.69, 53));
+        // BMI tra ve kieu double co truyen tham so
+        System.out.println(bmi2(1.69, 53));
 
+        // BMI tra ve kieu void
+        bmivoid();
     }
+
+    public static double bmi2 (double height, double weight){
+        return weight/(height*height);
+    }
+
+    public static void bmivoid() {
+        double canNang = 53;
+        double chieuCao = 1.69;
+        double bmical = canNang/(chieuCao*chieuCao);
+        System.out.println("Ket qua BMIvoid cua ban la " + bmical);
+    }
+
+    // Nhan xet: phuong thuc tra ve kieu double co truyen tham so la tot nhat, vi co the thay doi so lieu sau nay
     
 }
--- java-core-1/src/bmi.java
+++ java-core-1/src/bmi.java
@@ -1,18 +1,18 @@
-public class bmi {
+// public class bmi {
 
     
 
-    public static double bmi2 (double height, double weight){
-        return weight/(height*height);
-    }
+//     public static double bmi2 (double height, double weight){
+//         return weight/(height*height);
+//     }
 
-    public static void bmi1() {
+//     // public static void bmi1() {
 
-    double canNang = 53;
-    double chieuCao = 1.69;
-    double bmical = canNang/(chieuCao*chieuCao);
-    }
+//     // double canNang = 53;
+//     // double chieuCao = 1.69;
+//     // double bmical = canNang/(chieuCao*chieuCao);
+//     // }
     
 
    
-}
+// }
