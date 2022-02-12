5832f3511cf48cd4ae95ca5df5f01f8749d475c3
Kiên <fukz2803@gmail.com>
2022-02-12 18:03:49 +0700

    a

--- bai-tap/Project/src/Main.java
+++ bai-tap/Project/src/Main.java
@@ -1,12 +1,8 @@
 import Controller.Controller;
-import Model.Category;
-import Service.ProductService;
 
 public class Main {
     public static void main(String[] args) {
-//        Controller.mainMenu();
-        ProductService service = new ProductService();
-        service.sortCategoryByPriceLTH(Category.MAINBOARD);
+        Controller.mainMenu();
     }
 
 }
